c744d30543a9240b90e90d8c7d8b6045bfc8cc25
Nikita <nikitakokosss@gmail.com>
2021-03-29 22:32:57 +0300

    Third commit of ThirdLab with changing Controller,java ahd App.fxml

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import static javafx.scene.paint.Color.BLACK;
+import static javafx.scene.paint.Color.RED;
+
 public class Controller {
 
     @FXML
@@ -86,9 +89,8 @@ public class Controller {
     private int totalTeaNum = 0;
     private int totalMilkNum =0;
 
-    // Make coffee
-    @FXML
-    void click(ActionEvent event){
+    public void duplicateCode(int drinkNum)
+    {
         boolean correctness = true;
         if(coffeeCupNumber+coffeeWithMilkNumber>8)
         {
@@ -99,31 +101,44 @@ public class Controller {
             unexpectedText.setText("больше кофе не сделаешь)))))");
             correctness = false;
         }
+        if(teaCupNumber>10)
+        {
+            unexpectedText.setText("Много чая тоже вредно!");
+            correctness = false;
+        }
         if(studentNameTxtField.getText().isEmpty())
         {
             studentNameText.setText("ВВЕДИТЕ ИМЯ!!!!");
+            studentNameText.setFill(RED);
             correctness = false;
         }
         // Check coffee taste entering
-        if(tasteOfCoffeeTxtField.getText().isEmpty())
+        if((tasteOfCoffeeTxtField.getText().isEmpty())&&((drinkNum==1)||(drinkNum==0)))
         {
             tasteOfCoffeeText.setText("ВВЕДИТЕ ВКУС КОФЕ!!!");
+            tasteOfCoffeeText.setFill(RED);
             correctness = false;
         }
         // Check water temperature entering
-        if(waterTemperatureSlider.getValue()<91)
+        if((waterTemperatureSlider.getValue()<91)&&((drinkNum==1)||(drinkNum==0)))
         {
-            kettleTemperatureText.setText("ТЕМПЕРАТУРА СЛИШКОМ МАЛА!!!");
+            kettleTemperatureText.setText("Необходима температура не менее 91 гр.!!!");
+            kettleTemperatureText.setFill(RED);
             correctness = false;
         }
         if(correctness)
         {
             studentNameText.setText(studentNameTxtField.getText());
+            studentNameText.setFill(BLACK);
             totalCupVolume += (int)cupVolumeSlider.getValue();
-            cupVolumeText.setText("Общий объем " + String.valueOf(totalCupVolume)+"  мл.");
-            tasteOfCoffeeText.setText("Вкус кофе  " + tasteOfCoffeeTxtField.getText());
-            totalCoffeeNum+=(int)coffeeSlider.getValue();
-            quantityOfCoffeeText.setText("Общее кол-во кофе  " + String.valueOf(totalCoffeeNum)+"  ед.");
+            cupVolumeText.setText("Общий объем напитков " + String.valueOf(totalCupVolume)+"  мл.");
+            if((drinkNum==1)||(drinkNum==0))
+            {
+                tasteOfCoffeeText.setText("Вкус кофе  " +'"'+ tasteOfCoffeeTxtField.getText()+'"');
+                tasteOfCoffeeText.setFill(BLACK);
+                totalCoffeeNum+=(int)coffeeSlider.getValue();
+                quantityOfCoffeeText.setText("Общее кол-во кофе  " + String.valueOf(totalCoffeeNum)+"  ед.");
+            }
             totalSugarNum+=(int)sugarQuantitySlider.getValue();
             sugarQuantityText.setText("Общее кол-во сахара  " + String.valueOf(totalSugarNum)+ "  ед.");
             totalKettleVolume+=(int)kettleVolumeSlider.getValue();
@@ -131,106 +146,55 @@ public class Controller {
             averageKettleTemperature+=(int)waterTemperatureSlider.getValue();
             averageKettleTemperature/=2;
             kettleTemperatureText.setText("Средняя t воды  "+String.valueOf(averageKettleTemperature)+ "  градусов");
-
+            kettleTemperatureText.setFill(BLACK);
+            if(drinkNum==1)
+            {
+                totalMilkNum += 10;
+                resultCounterOfMilk.setText("Общее кол-во молока " + totalMilkNum+"  мл.");
+            }
             Coffee coffee = new Coffee(5*(int)coffeeSlider.getValue(),tasteOfCoffeeTxtField.getText());
+            Tea tea = new Tea(5, "Сортовой)");
             Sugar sugar = new Sugar(5*(int)sugarQuantitySlider.getValue());
             Cup cup = new Cup(100+((int)cupVolumeSlider.getValue()/10),(int)cupVolumeSlider.getValue(),"");
             Kettle kettle = new Kettle(200+((int)kettleVolumeSlider.getValue()/10),(int)kettleVolumeSlider.getValue(), (int)waterTemperatureSlider.getValue());
             Student student = new Student(studentNameTxtField.getText());
-            student.makeCoffee(sugar,coffee,kettle,cup,(int)waterTemperatureSlider.getValue());
-
-            coffeeCupNumber++;
-            resultCounterOfCoffeeCup.setText("Приготовлено  "+ coffeeCupNumber +"  ед. кофе без молока");
-        }
-        }
-        // Make coffee with milk
-        @FXML
-        void secondClick(ActionEvent event) {
-            boolean correctness = true;
-            if (coffeeCupNumber + coffeeWithMilkNumber > 8) {
-                unexpectedText.setText("МНОГО КОФЕ ВРЕДНО...");
+            if(drinkNum==0)
+            {
+                student.makeCoffee(sugar,coffee,kettle,cup,(int)waterTemperatureSlider.getValue());
+                coffeeCupNumber++;
+                resultCounterOfCoffeeCup.setText("Приготовлено  "+ coffeeCupNumber +"  ед. кофе без молока");
             }
-            if (coffeeCupNumber + coffeeWithMilkNumber > 13) {
-                unexpectedText.setText("больше кофе не сделаешь)))))");
-                correctness = false;
-            }
-            if (studentNameTxtField.getText().isEmpty()) {
-                studentNameText.setText("ВВЕДИТЕ ИМЯ!!!!");
-                correctness = false;
-            }
-            // Check coffee taste entering
-            if (tasteOfCoffeeTxtField.getText().isEmpty()) {
-                tasteOfCoffeeText.setText("ВВЕДИТЕ ВКУС КОФЕ!!!");
-                correctness = false;
-            }
-            // Check water temperature entering
-            if (waterTemperatureSlider.getValue() < 91) {
-                kettleTemperatureText.setText("ТЕМПЕРАТУРА СЛИШКОМ МАЛА!!!");
-                correctness = false;
-            }
-            if (correctness) {
-                studentNameText.setText(studentNameTxtField.getText());
-                totalCupVolume += (int) cupVolumeSlider.getValue();
-                cupVolumeText.setText("Общий объем " + String.valueOf(totalCupVolume) + "  мл.");
-                tasteOfCoffeeText.setText("Вкус кофе  " + tasteOfCoffeeTxtField.getText());
-                totalCoffeeNum += (int) coffeeSlider.getValue();
-                quantityOfCoffeeText.setText("Общее кол-во кофе  " + String.valueOf(totalCoffeeNum) + "  ед.");
-                totalSugarNum += (int) sugarQuantitySlider.getValue();
-                sugarQuantityText.setText("Общее кол-во сахара  " + String.valueOf(totalSugarNum) + "  ед.");
-                totalKettleVolume += (int) kettleVolumeSlider.getValue();
-                kettleVolumeText.setText("Общий объем чайника " + String.valueOf(totalKettleVolume) + "  мл.");
-                averageKettleTemperature += (int) waterTemperatureSlider.getValue();
-                averageKettleTemperature /= 2;
-                kettleTemperatureText.setText("Средняя t воды  " + String.valueOf(averageKettleTemperature) + "  градусов");
-                totalMilkNum += 10;
-                resultCounterOfMilk.setText("Общее кол-во молока " + totalMilkNum);
-                Coffee coffee = new Coffee(5 * (int) coffeeSlider.getValue(), tasteOfCoffeeTxtField.getText());
-                Sugar sugar = new Sugar(5 * (int) sugarQuantitySlider.getValue());
-                Cup cup = new Cup(100 + ((int) cupVolumeSlider.getValue() / 10), (int) cupVolumeSlider.getValue(), "");
-                Kettle kettle = new Kettle(200 + ((int) kettleVolumeSlider.getValue() / 10), (int) kettleVolumeSlider.getValue(), (int) waterTemperatureSlider.getValue());
-                Student student = new Student(studentNameTxtField.getText());
-
+            if(drinkNum==1)
+            {
                 student.makeCoffeeWithMilk(sugar, coffee, kettle, cup, (int) waterTemperatureSlider.getValue());
-
                 coffeeWithMilkNumber++;
                 resultCounterOfCoffeeWithMilk.setText("Приготовлено  " + coffeeWithMilkNumber + "  ед. кофе c молоком");
             }
+            if(drinkNum==2)
+            {
+                student.makeTea(sugar, kettle, cup, tea);
+                teaCupNumber++;
+                totalTeaNum += 5;
+                resultCounterOfTea.setText("Общее кол-во чая " + totalTeaNum + "  г");
+                resultCounterOfTeaCup.setText("Общее кол-во чашек чая " + teaCupNumber + "  ед");
+            }
+        }
+    }
+    // Make coffee
+    @FXML
+    void click(ActionEvent event){
+           duplicateCode(0);
         }
 
+        // Make coffee with milk
+        @FXML
+        void secondClick(ActionEvent event) {
+                duplicateCode(1);
+        }
         // Make tea
         @FXML
         void thirdClick(ActionEvent event){
-        boolean correctness = true;
-        if (waterTemperatureSlider.getValue()<80) {
-            kettleTemperatureText.setText("ТЕМПЕРАТУРА СЛИШКОМ МАЛА!!!");
-            correctness = false;
+            duplicateCode(2);
         }
-        if (studentNameTxtField.getText().isEmpty()) {
-            studentNameText.setText("ВВЕДИТЕ ИМЯ!!!!");
-            correctness = false;
-        }
-        if (correctness)
-        {
-            studentNameText.setText(studentNameTxtField.getText());
-            teaCupNumber++;
-            totalTeaNum += 5;
-            totalSugarNum += (int) sugarQuantitySlider.getValue();
-            sugarQuantityText.setText("Общее кол-во сахара  " + String.valueOf(totalSugarNum) + "  ед.");
-            totalKettleVolume += (int) kettleVolumeSlider.getValue();
-            kettleVolumeText.setText("Общий объем чайника " + String.valueOf(totalKettleVolume) + "  мл.");
-            averageKettleTemperature += (int) waterTemperatureSlider.getValue();
-            averageKettleTemperature /= 2;
-            kettleTemperatureText.setText("Средняя t воды  " + String.valueOf(averageKettleTemperature) + "  градусов");
-            Sugar sugar = new Sugar(5 * (int) sugarQuantitySlider.getValue());
-            Cup cup = new Cup(100 + ((int) cupVolumeSlider.getValue() / 10), (int) cupVolumeSlider.getValue(), "");
-            Kettle kettle = new Kettle(200 + ((int) kettleVolumeSlider.getValue() / 10), (int) kettleVolumeSlider.getValue(), (int) waterTemperatureSlider.getValue());
-            Student student = new Student(studentNameTxtField.getText());
-            Tea tea = new Tea(5, "Сортовой)");
-            student.makeTea(sugar, kettle, cup, tea);
-            resultCounterOfTea.setText("Общее кол-во чая " + totalTeaNum + "  г");
-            resultCounterOfTeaCup.setText("Общее кол-во чая " + teaCupNumber + "  ед");
-        }
-        }
-
     }
 
